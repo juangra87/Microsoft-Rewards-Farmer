67ff2a475d5e3d004f83743c664f8d35d0e16f49
JuanGa <juangabriel.perezdonaire@bestsecret.com>
2024-01-09 15:49:53 +0100

    - Loop on all the accounts, so the job is always running - Remove the initial shuffle of the accounts, so we execute them in order - Change range sleep time

--- src/searches.py
+++ src/searches.py
@@ -82,7 +82,6 @@ class Searches:
                 searchbar = self.webdriver.find_element(By.ID, "sb_form_q")
                 searchbar.send_keys(word)
                 searchbar.submit()
-                time.sleep(random.randint(200, 300))
                 return self.browser.utils.getBingAccountPoints()
             except TimeoutException:
                 logging.error("[BING] " + "Timeout, retrying in 5 seconds...")
